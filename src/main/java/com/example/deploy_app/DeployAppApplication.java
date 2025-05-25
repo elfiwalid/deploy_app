357248ec0ecfdac6d259d98f6a516f93bf1d5f0e
elfiwalid <walid11elfilali@gmail.com>
2025-05-25 01:24:57 +0100

    commit first version

---
+++ src/main/java/com/example/deploy_app/DeployAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.deploy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DeployAppApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "✅ Déployé avec succès via Docker + GitHub Actions + Azure!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployAppApplication.class, args);
+    }
+}
